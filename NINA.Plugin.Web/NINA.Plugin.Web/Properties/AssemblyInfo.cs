8be5eb07acae20872f4397ac13e33dac9bb89f2d
Tom Palmer <palmer.tom@gmail.com>
2022-01-20 16:19:18 -0500

    name change

--- NINA.Plugin.Web/NINA.Plugin.Web/Properties/AssemblyInfo.cs
+++ NINA.Plugin.Web/NINA.Plugin.Web/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("81B04674-EA65-4FE8-B79B-A77C1D209183")]
 
-[assembly: AssemblyTitle("NINA Web")]
-[assembly: AssemblyDescription("Embedded Web server providing acquisition session history details")]
+[assembly: AssemblyTitle("Web Session History Viewer")]
+[assembly: AssemblyDescription("Embedded Web server and app providing acquisition session history details")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tom Palmer @tcpalmer")]
 [assembly: AssemblyProduct("Web.NINAPlugin")]
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("FeaturedImageURL", "https://raw.githubusercontent.com/tcpalmer/nina.plugin.web/main/NINA.Plugin.Web/assets/web-plugin-icon.png?raw=true")]
 [assembly: AssemblyMetadata("ScreenshotURL", "https://raw.githubusercontent.com/tcpalmer/nina.plugin.web/main/NINA.Plugin.Web/assets/screenshot1.png?raw=true")]
 
-[assembly: AssemblyMetadata("LongDescription", @"The NINA Web plugin embeds a lightweight Web server that will provide access to details associated with your acquisition sessions.  A custom Web app is included to present the data.  If NINA is running and the plugin is enabled, you can view details from the current ('live') session as well as past sessions from any device on your local network.
+[assembly: AssemblyMetadata("LongDescription", @"The Web Session History Viewer embeds a lightweight Web server that will provide access to details associated with your acquisition sessions.  A custom Web app is included to present the data.  If NINA is running and the plugin is enabled, you can view details from the current ('live') session as well as past sessions from any device on your local network.
 
 The actual URLs to access the Web app will be displayed above once the plugin is installed:
 * Local Address: for a browser running on this computer
@@ -40,6 +40,7 @@ The local network address will use the IP of the computer running NINA.  Assumin
 * There will only be one session history file for each run of NINA.  Disabling and enabling the plugin itself won't start a new session. 
 * Click Show Console to see app status messages.  If this button changes color, click to see what the problem is.  The most common issue is that the Web app is still running but the plugin was disabled or NINA was stopped.  In that case, just close the browser window/tab.
 * If the Web app is in a weird state, just click reload in the browser.
+* Session history and Web server logs are stored in the WebPlugin directory under the main NINA directory.
 
 ## Network Setup
 
@@ -58,7 +59,7 @@ If this doesn't work you'll need to search around and figure out why.
 * [Web app source code](https://github.com/tcpalmer/nina.plugin.web.client)
 * [Change log](https://github.com/tcpalmer/nina.plugin.web/blob/main/CHANGELOG.md)
 
-The NINA Web Plugin is provided 'as is' under the terms of the [Mozilla Public License 2.0](https://github.com/tcpalmer/nina.plugin.web/blob/main/LICENSE.txt)
+The Web Session History Viewer is provided 'as is' under the terms of the [Mozilla Public License 2.0](https://github.com/tcpalmer/nina.plugin.web/blob/main/LICENSE.txt)
 ")]
 
 [assembly: ComVisible(false)]
