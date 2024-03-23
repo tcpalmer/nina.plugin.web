be45d2aa0cbdd9da6a182a3eb167f66de0a77e9b
Tom Palmer <palmer.tom@gmail.com>
2024-03-23 10:51:14 -0400

    Fixed problem with RE handling log rollover naming

--- NINA.Plugin.Web/NINA.Plugin.Web/Properties/AssemblyInfo.cs
+++ NINA.Plugin.Web/NINA.Plugin.Web/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyFileVersion("2.2.1.0")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
 [assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.2001")]
--- NINA.Plugin.Web/NINA.Plugin.Web/Utility/LogUtils.cs
+++ NINA.Plugin.Web/NINA.Plugin.Web/Utility/LogUtils.cs
@@ -11,14 +11,16 @@ namespace Web.NINAPlugin.Utility {
         }
 
         public static string GetLogFileRE() {
-            // NINA log files match yyyyMMdd-HHmmss-VERSION.PID-yyyyMMdd.log - see NINA.Core.Utility.Logger.
+            // NINA log files match yyyyMMdd-HHmmss-VERSION.PID-yyyyMM.log - see NINA.Core.Utility.Logger.
             // Note that daily log file rolling was added in Sept 2023 - that added the '-yyyyMMdd' to the end.
+            // It was then switched to monthly rolling in Oct 2023 so now '-yyyyMM' but RE will match either.
 
             string version = CoreUtil.Version;
             int processId = Environment.ProcessId;
-            return @"^\d{8}-\d{6}-" + $"{version}.{processId}" + @"-\d{8}.log$";
+            return @"^\d{8}-\d{6}-" + $"{version}.{processId}" + @"-\d{6,8}.log$";
         }
 
-        private LogUtils() { }
+        private LogUtils() {
+        }
     }
 }
